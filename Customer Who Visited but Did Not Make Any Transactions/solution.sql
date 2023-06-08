6bf8d5ea623d0968bdf8bb99b763cd8655a295a6
tdhwani <64563255+tdhwani@users.noreply.github.com>
2023-06-07 20:12:40 -0400

    Create solution.sql

---
+++ Customer Who Visited but Did Not Make Any Transactions/solution.sql
@@ -0,0 +1,10 @@
+select 
+  customer_id, 
+  count(Visits.visit_id) as count_no_trans 
+from 
+  Visits 
+  left join Transactions on Visits.visit_id = Transactions.visit_id 
+where 
+  transaction_id is NULL 
+group by 
+  customer_id;
